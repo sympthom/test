6450f52c2fd77d848f7bb05247c1310e3137f476
Martin Dahlstedt <martin.dahlstedt@crayon.com>
2020-03-26 18:11:52 +0100

    asd

--- New-ScaleUnitDeployment.ps1
+++ New-ScaleUnitDeployment.ps1
@@ -1,1 +1,9 @@
-New-AzureRmResourceGroupDeployment -Name $ResourceGroupName -ResourceGroupName $ResourceGroupName -Mode Incremental -TemplateFile $TemplateFile -TemplateParameterFile $TemplateParameterFile
+# Deploying scale unit case and exchange rate with bundled components
+# 
+# sof-case-we-dit
+#  sof: software
+#  case: scale unit Name
+#  we: region west europe
+#  dit: development integration test environment
+
+New-AzResourceGroupDeployment -Name 'deployment' -ResourceGroupName 'sof-case-we-dit' -Mode Incremental -TemplateFile 'sof-exchangerate.json' -TemplateParameterFile 'sof-exchangerate-dit-parameters.json'
